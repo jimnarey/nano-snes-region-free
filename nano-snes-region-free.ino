b22ed75b8328a85761871b9f7890ee2e1df2306e
Jim Narey <jimnarey@me.com>
2022-11-20 18:33:01 +0000

    Add LED flashing and finish main loop

--- nano-snes-region-free.ino
+++ nano-snes-region-free.ino
@@ -5,16 +5,16 @@
  *  +---------------------| USB |---------------------+
  *  |                     +-----+                     |
  *  | [ ]     PB5 D13/SCK        MISO/D12 PB4     [ ] |
- *  | [ ]         3.3V           MOSI/D11 PB3 PWM [ ] |
- *  | [ ]         V.ref     ___    SS/D10 PB2 PWM [ ] |
- *  | [ ]     PC0 A0       / N \       D9 PB1 PWM [ ] |
+ *  | [ ]         3.3V           MOSI/D11 PB3 PWM [B] |
+ *  | [ ]         V.ref     ___    SS/D10 PB2 PWM [G] |
+ *  | [ ]     PC0 A0       / N \       D9 PB1 PWM [R] |
  *  | [ ]     PC1 A1      /  A  \      D8 PB0     [ ] |
- *  | [ ]     PC2 A2      \  N  /      D7 PD7     [P] |
- *  | [ ]     PC3 A3       \_0_/       D6 PD6 PWM [I] |
+ *  | [ ]     PC2 A2      \  N  /      D7 PD7     [c] |
+ *  | [ ]     PC3 A3       \_0_/       D6 PD6 PWM [p] |
  *  | [ ]     PC4 A4/SDA               D5 PD5 PWM [S] |
- *  | [ ]     PC5 A5/SCL               D4 PD4     [C] |
+ *  | [ ]     PC5 A5/SCL               D4 PD4     [ ] |
  *  | [ ]         A6              INT1/D3 PD3 PWM [L] |
- *  | [ ]         A7              INT0/D2 PD2     [ ] |
+ *  | [ ]         A7              INT0/D2 PD2     [C] |
  *  | [ ]         5V                  GND         [ ] |
  *  | [ ]         RST                 RST PC6     [ ] |
  *  | [ ]         GND   5V MOSI GND   RX  PD0     [ ] |
@@ -32,6 +32,11 @@
  *  This says A6 & A7 can't at the HW level, so not even using port manipulation: https://arduino.stackexchange.com/questions/79303/unable-to-use-analog-pins-as-digital-inputs
  *  
  *  Helpful reference for reading a SNES controller: https://github.com/burks10/Arduino-SNES-Controller/blob/master/Arduino%20Micro%20SNES%20Controller/USBSNES/Arduino%20Micro%20SNES%20Controller.ino
+ * 
+ *  LED blended colours: 
+ *  - purple = 255, 0, 255
+ *  - yellow = 255, 125, 0
+ *
  */
 
 #define bIndex 0
@@ -58,6 +63,9 @@ int bluePin = 11;
 
 int PPUPin = 6;
 int CICPin = 7;
+
+int state60Hz = HIGH;
+int stateCIC = LOW;
 
 volatile int latchState = 0;
 volatile int cycleStart = 0;
@@ -86,26 +94,19 @@ void readClock() {
   
 }
 
-void set60Hz() {
-  digitalWrite(PPUPin, LOW);
-}
-
-void set50Hz() {
-  digitalWrite(PPUPin, HIGH);
-}
-
-void disableCIC() {
-  digitalWrite(CICPin, LOW);
-}
-
-void enableCIC() {
-  digitalWrite(CICPin, HIGH);
-}
-
 void setLedColour(int intensityRed, int intensityGreen, int intensityBlue) {
   analogWrite(redPin, intensityRed);
   analogWrite(greenPin, intensityGreen);
   analogWrite(bluePin, intensityBlue);
+}
+
+void flashLed(int intensityRed, int intensityGreen, int intensityBlue) {
+  for (i = 0; i < 3; ++i) {
+    setLedColour(intensityRed, intensityGreen, intensityBlue);
+    delay(300);
+    setLedColour(0, 0, 0);
+    delay(200);
+  }
 }
 
 void setup() {
@@ -117,33 +118,81 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(clockPin), readClock, CHANGE);
   pinMode(serialPin, INPUT);
 
+  pinMode(LED_BUILTIN, OUTPUT);
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
   pinMode(bluePin, OUTPUT);
 
   pinMode(PPUPin, OUTPUT);
   pinMode(CICPin, OUTPUT);
-  set60Hz();
-  disableCIC();
+  digitalWrite(PPUPin, state60Hz);
+  digitalWrite(CICPin, stateCIC);
+  setLedColour(255, 0, 0);
 
 }
 
 void loop() {
   // Serial.println(buttonsState[leftIndex]);
 
-  while (buttonsState[selectIndex] != 0 && buttonsState[lIndex] != 0) {
+  for (i = 0; i < 12; ++i) {
+    if (buttonsState[i] != 0) {
+      digitalWrite(LED_BUILTIN, HIGH);
+    }
+  }
 
-    if (buttonsState[bIndex] != 0) {
-      set60Hz();
-    } else if (buttonsState[aIndex] != 0) {
-      set50Hz();
-    } else if (buttonsState[yIndex] != 0) {
-      disableCIC();      
-    } else if (buttonsState[xIndex] != 0) {
-      enableCIC();
+  if (buttonsState[selectIndex] != 0) {
+
+    if buttonsState[leftIndex] != 0) {
+
+      if (buttonsState[bIndex] != 0) {
+        if (state60Hz == HIGH) {
+          // Enable 50Hz
+          state60Hz = LOW;
+          digitalWrite(PPUPin, state60Hz);
+          flashLed(0, 0, 255);
+        }
+      }
+
+      else if (buttonsState[yIndex] != 0) {
+        if (state60Hz == LOW) {
+          // Enable 60Hz
+          state60Hz = HIGH;
+          digitalWrite(PPUPin, state60Hz);
+          flashLed(0, 255, 0);
+        }
+      }
+
+      else if (buttonsState[aIndex] != 0) {
+        if (stateCIC == HIGH) {
+          // Disable CIC
+          stateCIC = LOW;
+          digitalWrite(CICPin, stateCIC);          
+          flashLed(255, 0, 255);
+        }      
+      }
+
+      else if (buttonsState[xIndex] != 0) {
+        if (state60Hz == LOW) {
+          // Enable CIC
+          stateCIC = HIGH;
+          digitalWrite(CICPin, stateCIC);
+          flashLed(255, 125, 0);
+        }
+      }
+
+      else if (buttonsState[xIndex] != 0) {
+        // Reset SNES
+      }
+
+      setLedColour(255, 0, 0);
+
     }
 
   }
+
   delay(100);
 
 }
+
+
+
